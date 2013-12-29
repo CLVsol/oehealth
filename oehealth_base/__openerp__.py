bf7c14b2704a7a5e6a00d450cc64d45b29d4cb8d
cvercelino <carlos.vercelino@clvsol.com>
2013-12-29 22:30:36 +0000

    Include oehealth_base

---
+++ oehealth_base/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+################################################################################
+#                                                                              #
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol                  #
+#                                                                              #
+# This program is free software: you can redistribute it and/or modify         #
+# it under the terms of the GNU Affero General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU Affero General Public License for more details.                          #
+#                                                                              #
+# You should have received a copy of the GNU Affero General Public License     #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+################################################################################
+
+{
+    'name': 'OpenERP Health - Base Module',
+    'version': '1.0.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'website': 'http://oehealth.org',
+    'description': '''
+    OeHealth: OpenERP Health, Pharmaceutical, and Hospital Information System
+    ''',
+    'depends': ['base',
+                ],
+    'data': [],
+    'init_xml': ['security/oehealth_base_security.xml',
+                 'oehealth_base_view.xml',
+                 ],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'active': False,
+}
